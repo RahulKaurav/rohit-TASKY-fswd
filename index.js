69870e0dd526ab56f34556bc9c0b6aa24b65fcf9
RahulKaurav <rkcrushit@gmail.com>
2021-06-24 02:03:06 +0530

    day 15 complete

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-const taskcontainer = document.querySelector(".task__container");
-console.log(taskcontainer);
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
 
-const savechanges = () => {
-    const taskdata = {
+const newCard = ({id, imageUrl, taskTitle, taskType, taskDescription}) => `<div class="col-md-6 col-lg-4" id=${id}>
+<div class="card">
+    <div class="card-header d-flex justify-content-end gap-2">
+        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+    </div>
+    <img src= ${imageUrl} class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${taskTitle}</h5>
+      <p class="card-text">${taskDescription}</p>
+      <span class="badge bg-primary">${taskType}</span>
+    </div>
+    <div class="card-footer text-muted">
+        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+    </div>
+  </div>
+</div>
+}`; /*
+const newCard = ({id, imageUrl ,taskTitle ,taskDescription ,taskType,}) => `<div class="col-md-6 col-lg-4" id=${id}>
+<div class="card text-center">
+    <div class="card-header d-flex justify-content-end gap-3"> 
+        <button type="button" class="btn btn-outline-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg></button>
+        <button type="button"  id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
+        <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i></button>
+    </div>
+    <img src=${imageUrl} class="card-img-top rounded-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${taskTitle}</h5>
+      <p class="card-text">${taskDescription}</p>
+      <span class="badge bg-primary">${taskType}</span>   <!-- here Primary(means normal,1st choice), Secondary(means different from primary, 2nd choice)-->
+      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+    <div class="card-footer text-muted">
+        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>            <!-- float-end(can be used for single content ) -->
+    </div>
+  </div>
+}`; */
+const saveChanges = () => {
+    const taskData = {
         id: '${Date.now()}',  //unique number for card id 
         imageUrl: document.getElementById("imageurl").value,
         taskTitle: document.getElementById("tasktitle").value,
@@ -10,5 +49,7 @@ const savechanges = () => {
         taskDescription: document.getElementById("taskdescription").value,
     };
 
-    console.log(taskdata);
+    const createNewCard = newCard(taskData);
+
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
 };
